88a5e1eaaa2c1f8e158417fa63b8eadde662ed9f
Lujaina Walid <90357532+Lujaina21@users.noreply.github.com>
2025-07-19 21:05:24 +0300

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -14,7 +14,7 @@ export default function Footer() {
                 Recipes
               </span>
             </a>
-            <span className="text-blue-600 font-bold text-2xl">Route</span>
+            <span className="text-blue-600 font-bold text-2xl">Meals</span>
           </div>
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <span className="block text-sm text-gray-500 text-center dark:text-gray-400">
